fe575d29f6cc2eca5e29d5019369ed4af7ff072b
Gars0128 <70803438+Gars0128@users.noreply.github.com>
2023-10-23 11:41:48 +0200

    ToDo Multitasking app for time and action managing

---
+++ Models/Category.cs
@@ -0,0 +1,10 @@
+﻿using System.Globalization;
+
+namespace ToDoDemo.Models
+{
+    public class Category
+    {
+        public string CategoryId { get; set; } = String.Empty;
+        public string Name { get; set; }=String.Empty;
+    }
+}
---
+++ Models/Status.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Authentication;
+
+namespace ToDoDemo.Models
+{
+    public class Status
+    {
+        public string StatusId { get; set; } = String.Empty;
+        public string Name { get; set; } = String.Empty;
+    }
+}
